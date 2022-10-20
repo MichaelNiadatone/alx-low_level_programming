a15641e024d3bdbf91ffad6ee8d3306cbe30fbd9
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-10-19 23:12:32 -0700

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main -prints 10 times the alphabet, in lowercase
- *
- * Return: Always 0
+ * print_alphabet x10 - print alphabet 10times
  */
 
 void print_alphabet_x10(void)
