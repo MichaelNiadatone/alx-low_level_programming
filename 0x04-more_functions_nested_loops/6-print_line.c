d1a5dac2ab2258849a322352388b08a47eb58e0b
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-10-20 16:57:57 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,11 +3,13 @@
 /**
  * print_line -  draws a straight line in the terminal
  * @n: the number of '_' to be printed
+ * Return: always 0
  */
 
 void print_line(int n)
 {
 	int len;
+
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
