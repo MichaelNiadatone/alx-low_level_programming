354c67d62d2b0d323565d1514464615fa69ad122
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-10-19 20:33:45 -0700

    checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower -checks for lowercase character
- *@c: the character 
+ *@c: the character
  * Return: 1 if the letter is lowercase, 0 if not
  */
 
 int _islower(int c)
 {
-	if (c >='a'; && c <='z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
