cd39c878985f09d75d0c9ea6528863d7ed9b3660
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-10-20 16:26:04 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,13 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (num = 0; num = 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-			{
-				_putchar((num / 10) = '0');
-			}
-			_putchar((num % 10) + '0');
+
+				_putchar('1');
+
+			_putchar(num % 10 + '0');
 		}
 		_putchar('\n');
 	}
