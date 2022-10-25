509206a69e9c0832775832932ff64fef787f66f0
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-10-25 16:21:28 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,14 +3,14 @@
 /**
  * puts2 - function that prints every other character of a string
  * @str: input
- * Return:print
+ * Return: print
  */
 
 void puts2(char *str)
 {
 	int i = 0;
 
-	for (str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
 			_putchar(str[i]);
