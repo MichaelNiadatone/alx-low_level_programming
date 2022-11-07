46b113d2ac4298d9d7343371d009fec1688d5d20
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-11-06 18:51:04 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int results = 0, num1, num2;
+	int result = 0, num1, num2;
 
 	if (argc != 3)
 	{
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	results = num1 * num2;
+	result = num1 * num2;
 
-	printf("%d\n", results);
+	printf("%d\n", result);
 	return (0);
 }
