962c5cad6ffb42b3e68832b3f479c0473c57ff98
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-11-06 17:07:02 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -a function to print its name
+ * @argc: argc parameter
+ * @argv: list of argc arrays
+ * Return: 0 success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
