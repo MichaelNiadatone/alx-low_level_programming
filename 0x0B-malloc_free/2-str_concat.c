3a3d4dd182fa6cca271edd5d843588d64a143700
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-11-08 17:17:33 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,59 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings
- * @s1: The first string
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1
- * Return: If the function fails - NULL.
+ * _strlen - counts and returns string length
+ * @s: that's the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	if (!*s)
+		return (0);
+	while (*s)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: one string
+ * @s2: the other
+ * Return: pointer to cat string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	unsigned int len, n, index;
+	char *new;
+	unsigned int i;
+	unsigned int j;
+	int total = 0;
 
-	if (s1 == NULL)
+	if (!s1)
 		s1 = "";
-
-	if (s2 == NULL)
+	if (!s2)
 		s2 = "";
-
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	total += _strlen(s1) + _strlen(s2);
+	new = malloc((total * sizeof(char)) + 1);
+	if (new == NULL)
+	{
 		return (NULL);
-
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		new[i] = s1[i];
+	}
+	for (j = 0; s2[j]; j++, i++)
+	{
+		new[i] = s2[j];
+	}
+	new[i] = '\0';
+	return (new);
 }
