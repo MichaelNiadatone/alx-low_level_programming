7dc6c5e99a1b43e827e7749f11da41454d4cf823
Michael_Niadatonne <michaelniadatone@gmail.com>
2022-11-13 12:36:29 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,19 +14,18 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (*s)
 	{
-		 for (index = 0; accept[index]; index++)
-		 {
-			 if (*s == accept[index])
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
 
-			 {
-				 bytes++;
-				 break
-			 }
-			 else if (accept[index + 1] == '\0')
-				 return (bytes);
-		 }
-
-		 s++;
+		s++;
 	}
 	return (bytes);
 }
